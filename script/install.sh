a9f204b758e96d82c8c815ac2f294978aa1b83b9
Bcap03 <3099869357@qq.com>
2020-02-13 07:52:14 -0500

    Ver1.1

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,6 @@
 apt-get update
 pkg install git python wget
-git clone https://github.com/Bcap03/Muxtools.git $PREFIX/share/
-wget -P /bin https://raw.githubusercontent.com/Geeks-alliance/Auto-Kali/master/scrips/auto-kali && chmod 777 /bin/auto-kaliecho "安装完成现在可以在任何地方输入'muxtools'来启动Muxtools了"
+git clone https://github.com/Bcap03/Muxtools.git $PREFIX/share/Muxtools
+wget -P /bin https://raw.githubusercontent.com/Bcap03/Muxtools/master/script/muxtools && chmod 777 /bin/muxtools
+echo "安装完成现在可以在任何地方输入'muxtools'来启动Muxtools了"
 
