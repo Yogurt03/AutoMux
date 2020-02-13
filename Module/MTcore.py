2eeb578300d0ec24a3eb31d58251601e0126771c
Bcap03 <3099869357@qq.com>
2020-02-13 07:35:04 -0500

    Ver1.0

--- Module/MTcore.py
+++ Module/MTcore.py
@@ -1,3 +1,53 @@
-## Module.py 是Ktools实现功能文件
+## MTcore.py 是Muxtools实现功能文件
 
-import os
+import os, sys
+
+Mt_banner =  """
+ ______                              _      
+|  ___ \             _              | |     
+| | _ | |_   _ _   _| |_  ___   ___ | | ___ 
+| || || | | | ( \ / )  _)/ _ \ / _ \| |/___)
+| || || | |_| |) X (| |_| |_| | |_| | |___ |
+|_||_||_|\____(_/ \_)\___)___/ \___/|_(___/ 
+
+"""                                       
+
+Mt_backmenu = """
+
+    [99]    返回主菜单
+    [00]    退出Muxtools
+
+"""
+
+def banner():
+    print(Mt_banner)
+
+def backmenu_option():
+    print(Mt_backmenu)
+
+def restart():
+	python = sys.executable
+	os.execl(python, python, * sys.argv)
+	curdir = os.getcwd()
+
+def install_lazymux():
+    os.system("git clone git clone https://github.com/Bcap03/Muxtools.git $HOME/")
+
+def qhd():
+    print("更换清华源中，请稍后...")
+    os.system("sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux stable main@' $PREFIX/etc/apt/sources.list")
+    os.system("apt-get update")
+
+def gfy():
+    print("更换官方源中，请稍后...")
+    os.system("rm -rf $PREFIX/etc/apt/sources.list")
+    os.system('echo "deb https://termux.org/packages/ stable main" >$PREFIX/etc/apt/sources.list')
+    os.system("apt-get update")
+
+def install_msf():
+    print("安装msf中，请稍候...")
+    os.system("pkg install unstable-repo -y && pkg install metasploit -y")
+
+def repair():
+    print("修复msf数据库中，请稍候...")
+    os.system("pg_ctl -D $PREFIX/var/lib/postgresql -l logfile start")
--- muxtools.py
+++ muxtools.py
@@ -1,3 +1,64 @@
-import os
-from Module.MTcore import *
 
+import os, sys
+from Module.MTcore import *
+from time import sleep as timeout
+
+
+def main():
+    banner()
+    print("    [1]  更换Termux源")
+    print("    [2]  安装Lazymux")
+    print("    [3]  安装Metasploit")
+    print("    [4]  修复msf数据库")
+    print("\n    [0]  退出Muxtoosl")
+    XXX = input("请输入要运行的指令 >>> ")
+    if XXX == "1": #更换Termux源
+        print("    [1] 更换官方源")
+        print("    [2] 更换清华大学源")
+        print("    [0] 返回主菜单")
+        XX = input("请输入要运行的命令 >>> ")
+        if XX == "1": #更换官方源
+            gfy()
+            print("更换成功，2秒后重启程序")
+            time.sleep(2)
+            restart()
+        elif XX == "2": #更换清华大学源
+            qhd()
+            print("更换成功，2秒后重启程序")
+            time.sleep(2)
+            restart()
+        elif XX == "0": #返回主程序
+            os.system("clear")
+            restart()
+        else:
+            print("输入错误请重新输入!")
+            timeout(2)
+            os.system("clear")
+            restart()
+    elif XXX == "2": #安装Lazymux
+        install_lazymux()
+        print("安装成功，2秒后重启程序")
+        time.sleep(2)
+        restart()
+    elif XXX == "3": #安装MSF
+        install_msf()
+        print("安装成功，2秒后重启程序")
+        time.sleep(2)
+        restart()
+    elif XXX == "4":
+        repair()
+        print("修复成功，2秒后重启程序")
+        time.sleep(2)
+        restart()
+    elif XXX == "0": #退出程序
+        sys.exit()
+    else:
+        print("输入错误请重新输入!")
+        timeout(2)
+        os.system("clear")
+        restart()
+
+
+
+if __name__ == "__main__":
+	main()
