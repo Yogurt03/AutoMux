906d544fb42f982bd13958468cafbd572fc2d8af
Bcap03 <60796531+Bcap03@users.noreply.github.com>
2020-02-13 21:09:51 +0800

    升级

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,5 @@
 apt-get update
-pkg install git python wget
+pkg install git python wget -y
 git clone https://github.com/Bcap03/Muxtools.git $PREFIX/share/Muxtools
 wget -P /bin https://raw.githubusercontent.com/Bcap03/Muxtools/master/script/muxtools && chmod 777 /bin/muxtools
 echo "安装完成现在可以在任何地方输入'muxtools'来启动Muxtools了"
