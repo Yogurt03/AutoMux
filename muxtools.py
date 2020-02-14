05bbbbd26fec00c9dbbe03f23a1baf7cd3084e95
Bcap03 <3099869357@qq.com>
2020-02-14 05:23:03 -0500

    更改功能显示方式

--- muxtools.py
+++ muxtools.py
@@ -7,17 +7,15 @@ from time import sleep as timeout
 def main():
     banner()
     print("    [1]  更换Termux源")
-    print("    [2]  安装Lazymux")
-    print("    [3]  安装Metasploit")
-    print("    [4]  修复msf数据库")
-    print("    [5]  安装Nmap")
-    print("    [6]  更新Muxtools")
+    print("    [2]  列出安装列表")
+    print("    [3]  修复MSF数据库")
+    print("    [4]  更新Muxtools")
     print("\n    [0]  退出Muxtoosl")
     XXX = input("请输入要运行的指令 >>> ")
     if XXX == "1": #更换Termux源
         print("    [1] 更换官方源")
         print("    [2] 更换清华大学源")
-        print("    [0] 返回主菜单")
+        print("\n    [0] 返回主菜单")
         XX = input("请输入要运行的命令 >>> ")
         if XX == "1": #更换官方源
             gfy()
@@ -37,35 +35,53 @@ def main():
             timeout(2)
             os.system("clear")
             restart()
-    elif XXX == "2": #安装Lazymux
-        install_lazymux()
-        print("安装完成，2秒后重启程序")
-        timeout(2)
-        restart()
-    elif XXX == "3": #安装MSF
-        install_msf()
-        print("安装完成，2秒后重启程序")
-        timeout(2)
-        restart()
-    elif XXX == "4":
+    elif XXX == "2": #列出安装列表
+        print("    [1]  安装Lazymux")
+        print("    [2]  安装Metasploit")
+        print("    [3]  安装Nmap")
+        print("\n    [0] 返回主菜单")
+        XX = input("请输入要运行的命令 >>> ")
+        if XX == "1":
+            install_lazymux()
+            print("安装成功，2秒后重启程序")
+            timeout(2)
+            restart()
+        elif XX == "2":
+            install_msf()
+            print("安装成功，2秒后重启程序")
+            timeout(2)
+            restart()           
+        elif XX == "3":
+            install_nmap()
+            print("安装成功，2秒后重启程序")
+            timeout(2)
+            restart()
+        elif XXX == "0" or "exit":
+            os.system("clear")
+            restart()
+        else:
+            print("输入错误请重新输入!")
+            timeout(2)
+            os.system("clear")
+            restart()
+    elif XXX == "3": #修复MSF数据库
         repair()
-        print("修复完成，2秒后重启程序")
+        print("安装成功，2秒后重启程序")
         timeout(2)
         restart()
-    elif XXX == "5":
-        install_nmap()
-        print("安装完成，2秒后重启程序")
+    elif XXX == "4": #更新Muxtools
+        print("功能还在开发中，2秒后重启程序")
         timeout(2)
+        os.system("clear")
         restart()
     elif XXX == "0" or "exit": #退出程序
         sys.exit()
+
     else:
         print("输入错误请重新输入!")
         timeout(2)
         os.system("clear")
         restart()
-
-
 
 if __name__ == "__main__":
 	main()
