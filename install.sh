86ea01977b08d41525d3410047a7f6e162be63ea
Bcap03 <60796531+Bcap03@users.noreply.github.com>
2020-02-14 13:03:34 -0600

    修复bug

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@ pkg install git python wget -y
 git clone https://github.com/Bcap03/Muxtools.git $PREFIX/share/Muxtools
 echo "python $PREFIX/share/Muxtools/muxtools.py" >$PREFIX/bin/muxtools
 chmod 777 $PREFIX/bin/muxtools
-clear
 echo "安装完成现在可以在任何地方输入'muxtools'来启动Muxtools了"
