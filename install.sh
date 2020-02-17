74e82dd770b87ef5f8a3f5e9479798cde38682e5
Bcap03 <bcap03@qq.com>
2020-02-18 03:21:33 +0800

    更新install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-apt-get update
-pkg install git python -y
+apt update
+apt install git python -y
 pip install termcolor
 git clone https://github.com/Bcap03/AutoMux.git $PREFIX/share/AutoMux
 echo "python $PREFIX/share/AutoMux/automux.py" >$PREFIX/bin/automux
 chmod 777 $PREFIX/bin/automux
-echo "安装完成现在可以在任何地方输入'automux'来启动AutoMux了"
+echo -e "\n\033[31m安装完成，请输入'automux'来启动AutoMux。\033[0m\n"
